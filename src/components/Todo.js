14afc822f649339896cbb7b0579ee2b86a3d4337
malsherer <mgracesherer@gmail.com>
2023-12-26 22:26:06 -0800

    worked on styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,87 +1,116 @@
 import React, { useState } from 'react';
 
 export default function Todo() {
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState('');
-    const [editingTaskIndex, setEditingTaskIndex] = useState(null); 
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [editingTaskIndex, setEditingTaskIndex] = useState(null); 
 
     const addTask = () => {
-        if (newTask.trim() !== '') {
-            if (editingTaskIndex !== null) {
-                const updatedTasks = [...tasks];
-                updatedTasks[editingTaskIndex] = { ...updatedTasks[editingTaskIndex], text: newTask };
-                setTasks(updatedTasks);
-                setEditingTaskIndex(null); 
+      if (newTask.trim() !== '') {
+        if (editingTaskIndex !== null) {
+          const updatedTasks = [...tasks];
+          updatedTasks[editingTaskIndex] = { ...updatedTasks[editingTaskIndex], text: newTask };
+          setTasks(updatedTasks);
+          setEditingTaskIndex(null); 
             } else {
-                setTasks([...tasks, { text: newTask, complete: false }]);
+              setTasks([...tasks, { text: newTask, complete: false }]);
             }
-            setNewTask('');
-        }
+          setNewTask('');
+      }
     };
 
     const removeTask = (index) => {
-        const updatedTasks = [...tasks];
-        updatedTasks.splice(index, 1);
-        setTasks(updatedTasks);
+      const updatedTasks = [...tasks];
+      updatedTasks.splice(index, 1);
+      setTasks(updatedTasks);
     };
 
     const completeTask = (index) => {
-        setTasks((prevTasks) => {
-            const updatedTasks = [...prevTasks];
-            updatedTasks[index] = {
-                ...updatedTasks[index],
-                complete: !updatedTasks[index].complete
-            };
-            return updatedTasks;
-        });
+      setTasks((prevTasks) => {
+        const updatedTasks = [...prevTasks];
+        updatedTasks[index] = {
+          ...updatedTasks[index],
+          complete: !updatedTasks[index].complete
+        };
+          return updatedTasks;
+      });
     };
 
     const editTask = (index) => {
-        setEditingTaskIndex(index);
-        setNewTask(tasks[index].text);
+      setEditingTaskIndex(index);
+      setNewTask(tasks[index].text);
     };
 
     const cancelEdit = () => {
-        setEditingTaskIndex(null);
-        setNewTask('');
+      setEditingTaskIndex(null);
+      setNewTask('');
     };
 
     return (
-        <div>
-            <h1>ToDo List</h1>
-            <div>
-                <input
-                    type='text'
-                    value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}
-                />
-                <button onClick={addTask}>{editingTaskIndex !== null ? 'Save Changes' : 'Add Task'}</button>
-                {editingTaskIndex !== null && (
-                    <button onClick={cancelEdit}>Cancel</button>
-                )}
-            </div>
-            <ul>
-                {tasks.map((task, index) => (
-                    <li
-                        key={index}
-                        className={task.complete ? 'completed' : 'not-completed'}
-                    >
-                        {editingTaskIndex === index ? (
-                            <input
-                                type='text'
-                                value={newTask}
-                                onChange={(e) => setNewTask(e.target.value)}
-                            />
-                        ) : (
-                            task.text
-                        )}
-                        <button onClick={() => completeTask(index)}>Mark as Complete</button>
-                        <button onClick={() => editTask(index)}>Edit</button>
-                        <button onClick={() => removeTask(index)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
+      <div className='main-body'>
+        <h1 className='app-header'>What would you like to get done today?</h1>
+        <div className='input-container'>
+          <input
+            type='text'
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className='task-input'
+						/>
+          <button 
+            className='form-btn'
+            id='add-task-btn' 
+            onClick={addTask}
+						>
+            {editingTaskIndex !== null ? 'Save Changes' : 'Add Task'}
+          </button>
+          	{editingTaskIndex !== null && (
+							<button
+							onClick={cancelEdit}
+							className='form-btn'
+							id='cancel-edit-btn'
+							>
+            Cancel
+          </button>
+            )}
         </div>
+        <ul className='task-list'>
+          {tasks.map((task, index) => (
+						<li
+            key={index}
+            className={task.complete ? 'completed' : 'not-completed'}
+						>
+          	{editingTaskIndex === index ? (
+							<input
+							type='text'
+							value={newTask}
+							onChange={(e) => setNewTask(e.target.value)}
+							className='edit-input'
+							/>
+							) : (
+								task.text
+								)}
+            <button 
+							onClick={() => completeTask(index)}
+							className='task-btn'
+							>
+								Mark as Complete
+						</button>
+            <button 
+							onClick={() => editTask(index)}
+							className='task-btn'
+							>
+								Edit
+						</button>
+            <button 
+							onClick={() => removeTask(index)}
+							className='task-btn'
+						>
+								Delete
+						</button>
+          </li>
+          ))}
+        </ul>
+      </div>
     );
 };
 
